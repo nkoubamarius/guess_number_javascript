b363cca15eeed1ce8398a0c387da4c550a717b8e
nkoubamarius <nkoubamarius@gmail.com>
2020-11-02 07:26:40 +0100

    Function to restart the game

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
  * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
- * TODO: Display the guess history using displayHistory() function
- * TODO: Use the initGame() function to restart the game
+ * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Save the guess history in a variable called guess
+ * DONE: Display the guess history using displayHistory() function
+ * DONE: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses 
@@ -56,6 +56,10 @@ function displayResult(numberGuess,correctNumber ){
 function initGame(){
     getRandomNumber();
     resetResultContent();
+    guesses=[];
+    displayHistory();
+    document.getElementById("history").innerHTML = "";
+    document.getElementById("number-guess").value="";
 }
 
 /**
